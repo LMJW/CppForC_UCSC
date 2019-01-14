08da3fd9920e2923b22e7910c21fdf0c6f0c10ae
LMJW <heysuperming@gmail.com>
2019-01-14 23:10:48 +1100

    improve

--- week23/conectivitygraph.cpp
+++ week23/conectivitygraph.cpp
@@ -21,6 +21,8 @@
  *  - Compute for a set of randomly generated graphs an average shortest path.
  */
 
+#include <stdlib.h>
+#include <time.h>
 #include <iostream>
 #include <vector>
 
@@ -28,28 +30,50 @@ using std::vector;
 
 class UndirectedGraph {
    public:
-    UndirectedGraph(int vertices);
-    ~UndirectedGraph() { release(); }
+    UndirectedGraph(int vertices, float percentage, int max_distance, int seed);
+    ~UndirectedGraph() { release(); };
     void release(){};
     void print();
     bool set_single_edge(int vertice_a, int vertice_b, int distance);
     int getdistance(int vertice_a, int vertice_b);
 
    private:
-    int size;
+    int size, seed, maxdistance;
+    float percentage;
     vector<vector<int>> data;
     bool is_not_outbound(int vertice_a, int vertice_b);
+    bool prob();
+    int rand_distance();
 };
 
-// initialize the number of vertices in the graph.
-// the graph is stored in a 2-d vector
-// initialize all the value as -1 for no connection
-// distance can be value that equals or greater than 0
-UndirectedGraph::UndirectedGraph(int vertices) : size(vertices) {
+// Allow user to customize seed to generate reproducable result
+// default seed equals to 0
+// we use distance = -1 to represent no connectivity
+UndirectedGraph::UndirectedGraph(int vertices,
+                                 float p,
+                                 int mdis = 100,
+                                 int n = 0)
+    : size(vertices), percentage(p), maxdistance(mdis), seed(n) {
+    if (percentage < 0) {
+        std::string errorMsg =
+            std::string("percentage must not be negative number.\n");
+        throw(std::runtime_error(errorMsg));
+    } else if (percentage > 1) {
+        std::string errorMsg =
+            std::string("percentage must be less or equal to 1.\n");
+        throw(std::runtime_error(errorMsg));
+    }
     data = vector<vector<int>>(size, vector<int>(size, -1));
-    // initial the distance from vertice i to i as 0;
     for (int i = 0; i < size; ++i) {
-        data[i][i] = 0;
+        for (int j = i; j < size; ++j) {
+            if (i == j) {
+                data[i][j] = 0;
+            } else if (prob()) {
+                int t = rand_distance();
+                data[i][j] = t;
+                data[j][i] = t;
+            }
+        }
     }
 }
 
@@ -81,7 +105,9 @@ int UndirectedGraph::getdistance(int vertice_a, int vertice_b) {
     if (is_not_outbound(vertice_a, vertice_b)) {
         return data[vertice_a][vertice_b];
     } else {
-        throw("Outbound error: vertices not exist!");
+        std::string errorMsg =
+            std::string("Outbound error: vertice indexes are out of bound.");
+        throw(std::runtime_error(errorMsg));
     }
 };
 // is_not_outbound:
@@ -98,12 +124,21 @@ bool UndirectedGraph::is_not_outbound(int vertice_a, int vertice_b) {
     return false;
 };
 
+// generate random true value based on the probability input
+// maximum percision is 4 digits after float operator
+// such as 99.99%
+//
+// example: 0.1 means there will be 10% chance of get true
+bool UndirectedGraph::prob() {
+    float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    return r < percentage;
+}
+
+int UndirectedGraph::rand_distance() {
+    return rand() % maxdistance + 1;
+}
+
 int main() {
-    UndirectedGraph g = UndirectedGraph(10);
-    g.print();
-    g.set_single_edge(1, 2, 10);
-    g.set_single_edge(3, 4, 5);
-    std::cout << g.getdistance(3, 4) << "\n";
-    std::cout << g.getdistance(4, 3) << "\n";
+    UndirectedGraph g = UndirectedGraph(10, 0.3, 50);
     g.print();
 };
