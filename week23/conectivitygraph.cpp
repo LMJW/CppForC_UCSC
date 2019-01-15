03289ac1763b5b2f32a3ca13eadec19dcb951e69
LMJW <heysuperming@gmail.com>
2019-01-16 00:07:09 +1100

    update

--- week23/conectivitygraph.cpp
+++ week23/conectivitygraph.cpp
@@ -1,6 +1,6 @@
 /**
  * Home work Week 2 & Week 3
- * Basic requirement
+ * Basic requirement:
  *
  *  - Write a set of constructors for declaring and initializing a graph.
  *
@@ -19,6 +19,17 @@
  *    less than the density.
  *
  *  - Compute for a set of randomly generated graphs an average shortest path.
+ *
+ *
+ * Turn in:
+ *  - Printout of program, 200 words on what you learned, and output
+ *    showing the average path length calculation. Use densities: 20% and 40% on
+ *    a graph of 50 nodes with a distance range of 1.0 to 10.0. To get an
+ *    average path length, compute the 49 paths:
+ *                         1 to 2, 1 to 3, 1 to 4, …, 1 to 50.
+ *    [In an instance where there is no path between 1 and n, omit that value
+ *    from the average. This should be very rare for the chosen density and size
+ *    in this homework.]
  */
 
 #include <stdlib.h>
@@ -34,16 +45,16 @@ class UndirectedGraph {
     ~UndirectedGraph() { release(); };
     void release(){};
     void print();
-    bool set_single_edge(int vertice_a, int vertice_b, int distance);
-    int getdistance(int vertice_a, int vertice_b);
+    float average_path_length();
 
    private:
-    int size, seed, maxdistance;
-    float percentage;
-    vector<vector<int>> data;
-    bool is_not_outbound(int vertice_a, int vertice_b);
+    int size, seed;
+    float maxdistance, percentage;
+    vector<vector<float>> data;
+
+    int find_min_dist(int, vector<float>, vector<std::pair<int, int>>);
     bool prob();
-    int rand_distance();
+    float rand_distance();
 };
 
 // Allow user to customize seed to generate reproducable result
@@ -51,7 +62,7 @@ class UndirectedGraph {
 // we use distance = -1 to represent no connectivity
 UndirectedGraph::UndirectedGraph(int vertices,
                                  float p,
-                                 int mdis = 100,
+                                 int mdis = 50,
                                  int n = 0)
     : size(vertices), percentage(p), maxdistance(mdis), seed(n) {
     if (percentage < 0) {
@@ -63,10 +74,10 @@ UndirectedGraph::UndirectedGraph(int vertices,
             std::string("percentage must be less or equal to 1.\n");
         throw(std::runtime_error(errorMsg));
     }
-    data = vector<vector<int>>(size, vector<int>(size, -1));
+    data = vector<vector<float>>(size, vector<float>(size, -1));
     for (int i = 0; i < size; ++i) {
         for (int j = i; j < size; ++j) {
-            if (i == j) {
+            if (i == j) {  // self distance should be 0
                 data[i][j] = 0;
             } else if (prob()) {
                 int t = rand_distance();
@@ -87,58 +98,61 @@ void UndirectedGraph::print() {
     }
 }
 
-// Set a single edge distance by giving vertice a, b and distance
-// return true if the distance is set successfully
-bool UndirectedGraph::set_single_edge(int vertice_a,
-                                      int vertice_b,
-                                      int distance) {
-    if (is_not_outbound(vertice_a, vertice_b)) {
-        data[vertice_a][vertice_b] = distance;
-        data[vertice_b][vertice_a] = distance;
-        return true;
+int UndirectedGraph::find_min_dist(int n,
+                                   vector<float> close,
+                                   vector<std::pair<int, int>> open) {
+    float next_dist = maxdistance + 1;
+    int idx;
+    for (size_t i = 0; i < open.size(); ++i) {
+        int start = open[i].first;
+        int end = open[i].second;
+        float d = close[start] + data[start][end];
+        if (d < next_dist) {
+            next_dist = d;
+            idx = end;
+        }
     }
-    return false;
-}
+};
 
 // Get the distance between two vertices
-int UndirectedGraph::getdistance(int vertice_a, int vertice_b) {
-    if (is_not_outbound(vertice_a, vertice_b)) {
-        return data[vertice_a][vertice_b];
-    } else {
-        std::string errorMsg =
-            std::string("Outbound error: vertice indexes are out of bound.");
-        throw(std::runtime_error(errorMsg));
+float UndirectedGraph::average_path_length() {
+    vector<float> closeset(size, -1);
+    vector<std::pair<int, int>> openset;
+
+    int c_node = 0;
+    closeset[c_node] = 0;
+
+    for (int j = 1; j < size; ++j) {
+        if (data[c_node][j] != -1) {
+            openset.push_back(std::make_pair(c_node, j));
+        }
+    }
+    while (openset.size() > 0) {
+        // given current node & close set & open set
+        // find the minimum & add to close set
+        c_node = find_min_dist(c_node, closeset, openset);
     }
 };
-// is_not_outbound:
-// check the given vertices number are not outbound of the 2d matrix
-// neither larger than the size and less than 0
-bool UndirectedGraph::is_not_outbound(int vertice_a, int vertice_b) {
-    if (0 <= vertice_a < size && 0 <= vertice_b < size) {
-        return true;
-    }
-    std::cout << "\nVertice out of bound! " << std::endl;
-    std::cout << "(" << vertice_a << "," << vertice_b << ") is larger than "
-              << size - 1 << "." << std::endl;
 
-    return false;
-};
-
-// generate random true value based on the probability input
-// maximum percision is 4 digits after float operator
-// such as 99.99%
-//
-// example: 0.1 means there will be 10% chance of get true
+// generate random true value based on the probability input when construct the
+// class. e.g: 0.1 means there will be 10% chance of get true
 bool UndirectedGraph::prob() {
+    // randomly generate a float number from 0 to 1
     float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
     return r < percentage;
 }
 
-int UndirectedGraph::rand_distance() {
-    return rand() % maxdistance + 1;
+float UndirectedGraph::rand_distance() {
+    // random generate a distance from 1 to maximum distance.
+    float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    if (r == 0.) {
+        return rand_distance();
+        // rerun random function if we get a 0 distance between different node
+    }
+    return r * maxdistance;
 }
 
 int main() {
-    UndirectedGraph g = UndirectedGraph(10, 0.3, 50);
+    UndirectedGraph g = UndirectedGraph(50, 0.2, 1);
     g.print();
 };
