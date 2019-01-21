52a591e8bc325589034548538a10fc5772be69a0
LMJW <heysuperming@gmail.com>
2019-01-21 21:24:30 +1100

    Refactoring code. 1. correct the index calculation; 2. add outbound condition justification; 3. change the index_for method to be protected method. This is important as this method will not be directly called by class user so it does not need to be a public method. However, if we set it as private, it will break some code if someone choose to inherit this graph class. So, set it as protected will allow the children classes to still use this method without breaking the code.

--- week23/graphv2.cpp
+++ week23/graphv2.cpp
@@ -1,3 +1,4 @@
+#include <exception>
 #include <iostream>
 #include <vector>
 
@@ -21,12 +22,7 @@ public:
 
     /// @return true if two vertices are linked by an edge
     bool adjacent(unsigned int x, unsigned int y) const {
-        return adj_[index(x, y)] > 0;
-    }
-
-    /// @return the index of 1-d vector given two verices in adjcent matrix
-    unsigned int index(unsigned int x, unsigned int y) const {
-        return x + n_v * y;
+        return adj_[index_for(x, y)] > 0;
     }
 
     vector<node> neighbours(node, node);
@@ -36,6 +32,16 @@ public:
     bool set_node_value(node, D);
     D get_edge_value(node, node);
     bool set_edge_value(node, node, D);
+
+protected:
+    /// @return the index of 1-d vector given two verices in adjcent matrix
+    unsigned int index_for(unsigned int x, unsigned int y) const {
+        /// have to consider the outbound error
+        if (x >= n_v || y >= n_v) {
+            throw invalid_argument("outbound of adjcent matrix!");
+        }
+        return x * n_v + y;
+    }
 
 private:
     const unsigned int n_v;
