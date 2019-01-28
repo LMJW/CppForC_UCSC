7866867c2590d57bb177d95c86703648ffae1bb2
LMJW <heysuperming@gmail.com>
2019-01-28 18:02:48 +1100

    course quiz

---
+++ term1_quiz/exam.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 2;
+    int b = -3;
+    int c = 2;
+    char ch = 'b';
+    bool t1 = false;
+    bool t2 = true;
+
+    cout << 1 << ": " << b + c * a << endl;
+    cout << 2 << ": " << c % 5 / 2 << endl;
+    cout << 3 << ": " << -a * c++ << endl;
+    cout << 3 << ": " << (t1 && t2) << endl;
+    cout << 3 << ": " << (ch += 2) << endl;
+
+}
